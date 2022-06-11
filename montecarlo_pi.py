78b8abe2c495c18e1d5e72c7b70e3a717d54c5cb
Vincent <bariboule@gmail.com>
2022-06-12 00:50:58 +0200

    Clean code according to python norms

--- montecarlo_pi.py
+++ montecarlo_pi.py
@@ -1,32 +1,36 @@
 from PRNG import *
 import matplotlib.pyplot as plt
 
+def main():
+    prng = prng_bbs()
 
-prng = prng_bbs()
+    xc, yc, xs, ys, pi = [], [], [], [], []
+    N = int(1e4)
+    pointsInSquare = 0
+    pointsInCircle = 0
+    for i in range(N):
+        x=randToInterval(prng(), -1, 1)
+        y=randToInterval(prng(), -1, 1)
+        if x**2+y**2 < 1:
+            pointsInCircle = pointsInCircle+1
+            xc.append(x); yc.append(y)
+        else :
+            xs.append(x); ys.append(y)
+        pointsInSquare = pointsInSquare + 1    
+        pi.append(4*pointsInCircle/pointsInSquare)
+        
+    fig = plt.figure()
+    ax = fig.add_subplot()
+    plt.plot(xc, yc,'.r')
+    plt.plot(xs, ys,'.b')
+    plt.title('2D Random Distribution')
+    ax.set_aspect('equal', adjustable='box')
+    plt.show()
 
-xc = []; yc = []; xs = []; ys =[]; pi = []
-N = int(1e4);
-pointsInSquare = 0
-pointsInCircle = 0
-for i in range(N):
-    x=randToInterval(prng(), -1, 1); y=randToInterval(prng(), -1, 1)
-    if x**2+y**2 < 1:
-        pointsInCircle = pointsInCircle+1
-        xc.append(x); yc.append(y)
-    else :
-        xs.append(x); ys.append(y)
-    pointsInSquare = pointsInSquare + 1    
-    pi.append(4*pointsInCircle/pointsInSquare)
-    
-fig = plt.figure()
-ax = fig.add_subplot()
-plt.plot(xc, yc,'.r')
-plt.plot(xs, ys,'.b')
-plt.title('2D Random Distribution')
-ax.set_aspect('equal', adjustable='box')
-plt.show()
 
+    plt.plot(pi)    
+    plt.title('Pi convergence')
+    plt.show()
 
-plt.plot(pi)    
-plt.title('Pi convergence')
-plt.show()
+if __name__ == '__main__':
+    main()
--- test_pnrg.py
+++ test_pnrg.py
@@ -2,31 +2,35 @@ from PRNG import *
 import matplotlib.pyplot as plt
 import time
 
-#####  Algo choice & initialization  #####
-# prng = prng_defaultPython()
-# prng = prng_lcg()
-prng = prng_bbs()
+def main():
+    #####  Algo choice & initialization  #####
+    # prng = prng_defaultPython()
+    # prng = prng_lcg()
+    prng = prng_bbs()
 
-#####  Test with plots ##### 
-## Histogram
-a = [];
-startTime = time.time()
-for i in range(10000):
-    a.append(prng());
-executionTime = (time.time() - startTime)
-print('Execution time in seconds: ' + str(executionTime))
-plt.hist(a, bins=50);
-plt.xlabel('Random outputs')
-plt.ylabel('# of outputs')
-plt.title('Random Distribution')
-plt.show();
+    #####  Test with plots ##### 
+    ## Histogram
+    a = []
+    startTime = time.time()
+    for i in range(10000):
+        a.append(prng())
+    executionTime = (time.time() - startTime)
+    print('Execution time in seconds: ' + str(executionTime))
+    plt.hist(a, bins=50)
+    plt.xlabel('Random outputs')
+    plt.ylabel('# of outputs')
+    plt.title('Random Distribution')
+    plt.show()
 
-## 2D-Plot
-x = [];y = [];
-for i in range(10000):
-    x.append(prng());
-    y.append(prng());
-    
-plt.plot(x, y,'.')
-plt.title('2D Random Distribution')
-plt.show()
+    ## 2D-Plot
+    x, y = [], []
+    for i in range(10000):
+        x.append(prng())
+        y.append(prng())
+        
+    plt.plot(x, y,'.')
+    plt.title('2D Random Distribution')
+    plt.show()
+
+if __name__ == '__main__':
+    main()
